d0788c514602d15f8aea6e556fb0f744dcdf4959
Phillip Johnston <phillip@embeddedartistry.com>
2019-10-09 12:14:09 -0700

    Reorganize files

--- examples/platform_logger.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLATFORM_LOGGER_HPP_
-#define PLATFORM_LOGGER_HPP_
-
-#include <circular_buffer_logger.hpp>
-
-using PlatformLogger =
-	PlatformLogger_t<CircularLogBufferLogger<1024>>;
-
-#endif
