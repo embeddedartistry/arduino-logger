3339e8c7a77c4d7deb3fe644b831032ba572e408
Phillip Johnston <phillip@embeddedartistry.com>
2021-04-19 10:33:51 -0700

    Auto-formatting update.

--- src/AVRSDRotationalLogger.h
+++ src/AVRSDRotationalLogger.h
@@ -181,18 +181,19 @@ class AVRSDRotationalLogger final : public LoggerBase
 		const char* buffer = log_buffer_.storage();
 
 		/** Note on const_cast
-		*
-		* The circular buffer returns a const char* for the underlying storage,
-		* but the AVR EEPROM library requires that a char* is passed to write().
-		* We're not modifying the buffer, so I'm const casting here.
-		*/
+		 *
+		 * The circular buffer returns a const char* for the underlying storage,
+		 * but the AVR EEPROM library requires that a char* is passed to write().
+		 * We're not modifying the buffer, so I'm const casting here.
+		 */
 
 		if((head < tail) || ((tail > 0) && (log_buffer_.size() == log_buffer_.capacity())))
 		{
 			// we have a wraparound case
 			// We will write from buffer[tail] to buffer[size] in one go
 			// Then we'll reset head to 0 so that we can write 0 to tail next
-			bytes_written = file_.write(const_cast<char*>(&buffer[tail]), log_buffer_.capacity() - tail);
+			bytes_written =
+				file_.write(const_cast<char*>(&buffer[tail]), log_buffer_.capacity() - tail);
 			bytes_written += file_.write(const_cast<char*>(buffer), head);
 		}
 		else
